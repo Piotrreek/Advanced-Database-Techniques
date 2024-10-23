be6f98ad2de54ce3b106e89720aaca7b1c671a1e
Piotr Gębalski <piotrgebalski5@gmail.com>
2024-10-23 17:17:43 +0200

    Add benchmarks for select

--- AdvancedDatabaseTechniques/Program.cs
+++ AdvancedDatabaseTechniques/Program.cs
@@ -4,4 +4,6 @@ using BenchmarkDotNet.Running;
 // BenchmarkRunner.Run<DatabaseBulkInsertComparison>();
 // BenchmarkRunner.Run<DatabaseInsertComparison>();
 // BenchmarkRunner.Run<DatabaseDeleteComparison>();
-BenchmarkRunner.Run<DatabaseUpdateComparison>();
+// BenchmarkRunner.Run<DatabaseUpdateComparison>();
+// BenchmarkRunner.Run<DatabaseSelectComparison>();
+BenchmarkRunner.Run<DatabaseSelectWithIndexComparison>();
